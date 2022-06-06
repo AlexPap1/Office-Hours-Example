bbe6e3b7989aab44d233eaebc13b3586644923fb
AlexPap <arpappagallo@gmail.com>
2022-06-06 10:13:10 -0400

    change post-url to post-content. Not regonized as coumn :(

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -40,14 +40,16 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-//FIND ONE???
 router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
     attributes: [
       'id',
-      'post_url',
       'title',
-      'created_at'
+      'created_at',
+      'post_content'
     ],
     include: [
       {
@@ -65,20 +67,23 @@ router.get('/edit/:id', withAuth, (req, res) => {
     ]
   })
     .then(dbPostData => {
-      if (dbPostData) {
-        const post = dbPostData.get({ plain: true });
-        
-        res.render('edit-post', {
-          post,
-          loggedIn: true
-        });
-      } else {
-        res.status(404).end();
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
       }
-    })
-    .catch(err => {
-      res.status(500).json(err);
+
+      const post = dbPostData.get({ plain: true });
+        
+      res.render('edit-post', {
+        post,
+        loggedIn: true
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
     });
-});
+      
 
 module.exports = router;
