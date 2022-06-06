53cb130db91e22680926508bb6979ccf1dc43519
AlexPap <arpappagallo@gmail.com>
2022-06-05 22:53:09 -0400

    removed additional vote content, spearated signup js from login js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,22 +1,19 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // get all posts for dashboard
 router.get('/', withAuth, (req, res) => {
-  console.log(req.session);
-  console.log('======================');
   Post.findAll({
     where: {
       user_id: req.session.user_id
     },
     attributes: [
       'id',
-      'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'post_content'
     ],
     include: [
       {
@@ -43,14 +40,14 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+//FIND ONE???
 router.get('/edit/:id', withAuth, (req, res) => {
   Post.findByPk(req.params.id, {
     attributes: [
       'id',
       'post_url',
       'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'created_at'
     ],
     include: [
       {
