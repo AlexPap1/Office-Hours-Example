917b6e24c30abfdba374eeda7fbcfb6bc0d6137f
AlexPap <arpappagallo@gmail.com>
2022-06-05 21:47:18 -0400

    add module 14.1 to repo

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const session = require('express-session');
 const exphbs = rrquire('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./controllers');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
